c1a9ae1cb072274dfd31cd061722a3954788cfcd
Dominik Przybyl <dominik.przybyl@wundermanthompson.com>
2024-06-06 02:11:35 +0200

    minor

--- sdk/go/aem/doc.go
+++ sdk/go/aem/doc.go
@@ -1,2 +1,2 @@
-// Easily manage AEM instances in the cloud without a deep dev-ops knowledge
+// Easily manage Adobe Experience Manager (AEM) instances in the cloud without a deep dev-ops knowledge
 package aem
